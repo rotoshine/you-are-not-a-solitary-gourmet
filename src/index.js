00339573539bcc09d4c7ea62d2890901dad11d36
Lucas <lucas.han@streami.co>
2018-06-28 20:14:29 +0900

    Create party store.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import 'moment/locale/ko'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'mobx-react'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import stores from './stores'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render((
+  <Provider {...stores}>
+    <App />
+  </Provider>
+), document.getElementById('root'))
 registerServiceWorker();
