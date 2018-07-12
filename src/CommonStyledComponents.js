09bb4bdbc17da38aa52ab8ea6583b8a96e9046a9
rotoshine <rotoshine@gmail.com>
2018-07-12 16:44:49 +0900

    styled component 적용

---
+++ src/CommonStyledComponents.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Overlay = styled.div`
+  position: fixed;
+  z-index: 10;
+  background: rgba(0,0,0,0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+`
---
+++ src/PartyStyledComponents.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const PartyTitle = styled.h2`
+  margin-top: 1rem;
+  font-size: 2.4rem;
+`
+
+export const PartyJoinnerGroup = styled.span`
+  width: 100%;
+  height: 100%;
+
+  em {
+    display:
+  }
+`
+
+export const PartyTag = styled.span`
+  width: 60px;
+  background: orange;
+  padding: 6px 10px;
+  margin-right: 10px;
+  line-height: 26px;
+  font-size: 1.4rem;
+  border-radius: 2px;
+`
+
+export const PartyJoinners = styled.div`
+  margin: 0.6rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const PartyJoinnerPhoto = styled.div`
+  display: flex;
+
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    border: 1px solid #ccc;
+    margin-right: 10px;
+  }
+`
+
+export const PartyItemInfoText = styled.p`
+  margin-bottom: 0.4rem;
+  font-size: 1.6rem;
+  color: grey;
+`
