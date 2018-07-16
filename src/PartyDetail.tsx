e3b4ad8608e448b80d374efdf9f6de9378c88b56
dahe oh <dahe.oh@gmail.com>
2018-07-17 00:57:07 +0900

    fix style

--- src/PartyDetail.tsx
+++ src/PartyDetail.tsx
@@ -27,17 +27,14 @@ const PartyDetailGroup = styled.div`
   width: 600px;
   background: white;
   padding: 4rem 3rem;
-  position: relative;
+  position: absolute;
+  top: 10rem;
   border-radius: 6px;
-  max-height: 90%;
-  overflow-y: scroll;
 
   @media screen and (max-width: 600px) {
     border-radius: 0;
     width: 100%;
-    height: 100%;
-    overflow-y: scroll;
-    max-height: 100%;
+    top: 0;
   }
 `
 const PartyDetailHeader = styled.div`
@@ -113,11 +110,6 @@ export default class PartyDetail extends React.Component<Props> {
                   >수정</HeaderButton>
                 </Link>
               )}
-              <Link to="/">
-                <HeaderButton
-                  className="btn btn-sm btn-danger"
-                >닫기</HeaderButton>
-              </Link>
             </div>
           </PartyDetailHeader>
           <div className="card-text">
